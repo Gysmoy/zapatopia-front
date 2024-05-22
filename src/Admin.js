81f2f98186b8b4d0b8e4f2c590c4a04dba13ebbe
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-05-22 16:27:57 -0500

    NTS

--- src/Admin.js
+++ src/Admin.js
@@ -5,15 +5,17 @@ import CategoriesPage from "./Pages/Admin/CategoriesPage"
 import ProductsPage from "./Pages/Admin/ProductsPage"
 import UsersPage from "./Pages/Admin/UsersPage"
 import TrazabilityPage from "./Pages/Admin/TrazabilityPage"
+import WarehousePage from "./Pages/Admin/WarehousePage"
 
 const Admin = () => {
     return (
       <Routes>
         <Route path="/" element={<DashboardPage />} />
         <Route path="/trazability" element={<TrazabilityPage />} />
+        <Route path="/products" element={<ProductsPage />} />
         <Route path="/brands" element={<BrandsPage />} />
         <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/warehouse" element={<WarehousePage />} />
         <Route path="/users" element={<UsersPage />} />
       </Routes>
     )
