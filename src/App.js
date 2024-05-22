f061f1868c3ff6269806260808a5a350f7c70bf8
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-05-22 13:12:20 -0500

    NTS

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartPage from './Pages/CartPage';
+import HomePage from './Pages/HomePage';
+import PageNotFound from './Pages/PageNotFound';
+import Admin from './Admin';
+import FiltersPage from './Pages/FiltersPage';
+import LoginPage from './Pages/LoginPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        // Public routes
+        <Route path="/" element={<HomePage />} />
+        <Route path="/filters" element={<FiltersPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        
+        // Admin routes
+        <Route path="/admin/*" element={<Admin />} />
+
+        // Page not found
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Pages/Admin/TrazabilityPage.jsx
@@ -0,0 +1,11 @@
+import BaseTemplate from "../../BaseTemplate"
+
+const TrazabilityPage = () => {
+    return (
+        <BaseTemplate>
+            <h1>Trazabilidad</h1>
+        </BaseTemplate>
+    )
+}
+
+export default TrazabilityPage
