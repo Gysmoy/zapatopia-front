72c18a24b44c85c46fe36c4452024b19aaec64ef
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-05-22 18:33:23 -0500

    NTS

--- src/Pages/Admin/ProductsPage.jsx
+++ src/Pages/Admin/ProductsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 import BaseTemplate from "../../BaseTemplate"
-import CategoriesRest from "../../Rest/CategoriesRest"
+import ProductsRest from "../../Rest/ProductsRest"
 import Modal from "../../Components/Modal"
 import Swal from "sweetalert2"
 import moment from 'moment-timezone'
@@ -9,139 +9,177 @@ moment.tz.setDefault("America/Lima")
 moment.locale("es")
 
 const ProductsPage = () => {
-    const [categories, setCategories] = useState([])
-    const [modalTittle, setModalTittle] = useState('Agregar categoria')
-    const [id, setId] = useState(null)
+  const [products, setProducts] = useState([])
+  const [modalTittle, setModalTittle] = useState('Agregar producto')
+  const [id, setId] = useState(null)
 
-    const modalRef = useRef()
-    const categoryRef = useRef()
-    const descriptionRef = useRef()
+  const modalRef = useRef()
+  const categoryRef = useRef()
+  const brandref = useRef()
+  const nameRef = useRef()
+  const purchasePriceRef = useRef()
+  const sizeRef = useRef()
+  const colorRef = useRef()
+  const genderRef = useRef()
+  const generalStockRef = useRef()
 
-    const $ = window.$
+  const $ = window.$
 
-    useEffect(() => {
-        document.title = "Categorias"
-        loadCategories()
-    }, [null])
+  useEffect(() => {
+    document.title = "Productos"
+    loadProducts()
+  }, [null])
 
-    const loadCategories = () => {
-        CategoriesRest.all().then((data) => {
-            setCategories(data)
-        })
+  const loadProducts = () => {
+    ProductsRest.all().then((data) => {
+      setProducts(data)
+    })
+  }
+
+  const resetForm = () => {
+    modalRef.current.reset()
+  }
+
+  const onModalSubmit = (e) => {
+    e.preventDefault()
+    const request = {
+      id: id || undefined,
+      categoria: {
+        id: categoryRef.current.value
+      },
+      marca: {
+        id: brandref.current.value
+      },
+      nombre: nameRef.current.value,
+      precioCompra: purchasePriceRef.current.value,
+      talla: sizeRef.current.value,
+      color: colorRef.current.value,
+      genero: genderRef.current.value,
+      stockGeneral: generalStockRef.current.value
     }
+    ProductsRest.save(request).then(() => {
+      resetForm()
+      $(modalRef.current).modal('hide')
+      loadProducts()
+    })
+  }
 
-    const resetForm = () => {
-        resetForm()
+  const onOpenModal = () => {
+    setModalTittle('Agregar producto')
+    resetForm()
+    setId(null)
+    $(modalRef.current).modal('show')
+  }
+
+  const onEditClicked = (product) => {
+    setModalTittle('Editar producto')
+    resetForm()
+    setId(product.id)
+    categoryRef.current.value = product.categoria
+    brandref.current.value = product.marca
+    nameRef.current.value = product.nombre
+    purchasePriceRef.current.value = product.precioCompra
+    sizeRef.current.value = product.talla
+    colorRef.current.value = product.color
+    genderRef.current.value = product.genero
+    generalStockRef.current.value = product.stockGeneral
+    $(modalRef.current).modal('show')
+  }
+
+  const onDeleteClicked = async (id) => {
+
+    const { isConfirmed } = await Swal.fire({
+      title: '¿Estás seguro de eliminar el producto?',
+      text: "No podrás revertir esto!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+    })
+
+    if (isConfirmed) {
+      ProductsRest.delete(id).then(() => {
+        loadProducts()
+      })
     }
-
-    const onModalSubmit = (e) => {
-        e.preventDefault()
-        const request = {
-            id: id || undefined,
-            categoria: categoryRef.current.value,
-            descripcion: descriptionRef.current.value
-        }
-        CategoriesRest.save(request).then(() => {
-            resetForm()
-            $(modalRef.current).modal('hide')
-            loadCategories()
-        })
-    }
-
-    const onOpenModal = () => {
-        setModalTittle('Agregar categoria')
-        resetForm()
-        setId(null)
-        $(modalRef.current).modal('show')
-    }
-
-    const onEditClicked = (category) => {
-        setModalTittle('Editar categoria')
-        resetForm()
-        setId(category.id)
-        categoryRef.current.value = category.categoria
-        descriptionRef.current.value = category.descripcion
-        $(modalRef.current).modal('show')
-    }
-
-    const onDeleteClicked = async (id) => {
-
-        const { isConfirmed } = await Swal.fire({
-            title: '¿Estás seguro de eliminar la categoria?',
-            text: "No podrás revertir esto!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-        })
-
-        if (isConfirmed) {
-            CategoriesRest.delete(id).then(() => {
-                loadCategories()
-            })
-        }
-    }
+  }
 
 
-    return (
-        <BaseTemplate title="Categorias">
-            <div className="row">
-                <div className="col-12">
-                    <div className="card">
-                        <div className="card-header d-flex align-items-center justify-content-between">
-                            <h4 className="header-title mt-0 mb-0">Lista de categorias</h4>
-                            <div>
-                                <button className="btn btn-xs btn-primary" onClick={onOpenModal}>Agregar</button>
-                            </div>
-                        </div>
-                        <div className="card-body">
-                            <div className="table-responsive">
-                                <table className="table table-centered table-bordered mb-0" id="btn-editable">
-                                    <thead className="table-light">
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Categoria</th>
-                                            <th>Descripción</th>
-                                            <th>Fecha de actualización</th>
-                                            <th>Acción</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        {categories.map((category) => {
-                                            const { id, categoria, descripcion, fechaModificacion } = category
-                                            return <tr key={`category-${id}`}>
-                                                <td>{id}</td>
-                                                <td>{categoria}</td>
-                                                <td>{descripcion}</td>
-                                                <td>{moment(fechaModificacion).format('YYYY-MM-DD HH:mm:ss')}</td>
-                                                <td>
-                                                    <div className="d-flex gap-1">
-                                                        <button className="btn btn-xs btn-info" onClick={() => onEditClicked(category)}>Editar</button>
-                                                        <button className="btn btn-xs btn-danger" onClick={() => onDeleteClicked(id)}>Eliminar</button>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        })}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <BaseTemplate title="Productos">
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div className="card-header d-flex align-items-center justify-content-between">
+              <h4 className="header-title mt-0 mb-0">Lista de productos</h4>
+              <div>
+                <button className="btn btn-xs btn-primary" onClick={onOpenModal}>Agregar</button>
+              </div>
             </div>
-            <Modal title={modalTittle} modalRef={modalRef} handleSubmit={onModalSubmit}>
-                <div className="form-group mb-2">
-                    <label htmlFor="category">Categoria</label>
-                    <input ref={categoryRef} name="category" type="text" className="form-control" required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="description">Descripción</label>
-                    <textarea ref={descriptionRef} name="description" className="form-control" rows={3} required />
-                </div>
-            </Modal>
-        </BaseTemplate>
-    )
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-centered table-bordered mb-0" id="btn-editable">
+                  <thead className="table-light">
+                    <tr>
+                      <th>ID</th>
+                      <th>Categoria</th>
+                      <th>Marca</th>
+                      <th>Nombre</th>
+                      <th>Precio Compra</th>
+                      <th>Talla</th>
+                      <th>Color</th>
+                      <th>Genero</th>
+                      <th>Stock General</th>
+                      <th>Fecha de actualización</th>
+                      <th>Acción</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {products.map((product) => {
+                      const { id, categoria, marca, nombre, precioCompra, talla, color, genero, stockGeneral, fechaModificacion } = product
+                      return <tr key={`product-${id}`}>
+                        <td>{id}</td>
+                        <td>{categoria}</td>
+                        <td>{marca}</td>
+                        <td>{nombre}</td>
+                        <td>{precioCompra}</td>
+                        <td>{talla}</td>
+                        <td>{color}</td>
+                        <td>{genero}</td>
+                        <td>{stockGeneral}</td>
+                        <td>{moment(fechaModificacion).format('YYYY-MM-DD HH:mm:ss')}</td>
+                        <td>
+                          <div className="d-flex gap-1">
+                            <button className="btn btn-xs btn-info" onClick={() => onEditClicked(product)}>Editar</button>
+                            <button className="btn btn-xs btn-danger" onClick={() => onDeleteClicked(id)}>Eliminar</button>
+                          </div>
+                        </td>
+                      </tr>
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal title={modalTittle} modalRef={modalRef} handleSubmit={onModalSubmit}>
+        <div className="form-group mb-2">
+          <label htmlFor="category">Categoria</label>
+          <input ref={categoryRef} name="category" type="text" className="form-control" required />
+        </div>
+        <div className="form-group mb-2">
+          <label htmlFor="category">Categoria</label>
+          <input ref={categoryRef} name="category" type="text" className="form-control" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Descripción</label>
+          
+        </div>
+      </Modal>
+    </BaseTemplate>
+  )
 }
 
 export default ProductsPage
